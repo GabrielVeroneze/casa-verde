2d7de978979b4caf8dec415f77835dab8b460ea3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-26 16:56:31 -0300

    :sparkles: feat: Adiciona mensagem de alerta para o usuário

--- src/hooks/useValidarEmail.ts
+++ src/hooks/useValidarEmail.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export const useValidarEmail = () => {
-    const [erroEmail, setErrosEmail] = useState<string>()
+    const [erroEmail, setErrosEmail] = useState<string>('')
 
     const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
@@ -17,6 +17,10 @@ export const useValidarEmail = () => {
             setErrosEmail('Por favor, insira um e-mail válido.')
             return
         }
+
+        alert(
+            `Obrigado pela sua assinatura, você receberá nossas novidades no e-mail ${emailUsuario}.`
+        )
     }
 
     return {
