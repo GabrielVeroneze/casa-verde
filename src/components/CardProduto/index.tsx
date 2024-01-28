a65ba8d4ce88a60125acd8edac98b78164e65dd0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-28 17:37:51 -0300

    :boom: fix: Corrige exibição da imagem e preço

--- src/components/CardProduto/index.tsx
+++ src/components/CardProduto/index.tsx
@@ -12,13 +12,13 @@ const CardProduto = ({ name, preco, img, ordem }: CardProdutoProps) => {
         <div
             className={styles.card}
             style={{
-                backgroundImage: `url('images/produto/${img}')`,
+                backgroundImage: `url('images/produtos/${img}.png')`,
                 order: ordem,
             }}
         >
             <div className={styles.conteudo}>
                 <h3 className={styles.titulo}>{name}</h3>
-                <p className={styles.preco}>R$ {preco}</p>
+                <p className={styles.preco}>R$ {preco.toFixed(2)}</p>
                 <button className={styles.botao}>Comprar</button>
             </div>
         </div>
