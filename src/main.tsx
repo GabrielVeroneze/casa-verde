f4b3f485f23d6867f5afac049b263e4d8ea144b1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-01 22:27:14 -0300

    :sparkles: feat: Utiliza componente para envolver todas rotas

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
+import { RecoilRoot } from 'recoil'
 import AppRoutes from '@/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <AppRoutes />
+        <RecoilRoot>
+            <AppRoutes />
+        </RecoilRoot>
     </React.StrictMode>
 )
