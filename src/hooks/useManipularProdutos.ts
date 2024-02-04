8fe8bd5a16f99a35591fdbbc2ee54716872dcd50
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-03 23:13:59 -0300

    :sparkles: feat: Cria lógica para filtrar os produtos

--- src/hooks/useManipularProdutos.ts
+++ src/hooks/useManipularProdutos.ts
@@ -8,6 +8,22 @@ export const useManipularProdutos = () => {
     const { produtos } = useListaProdutos()
     const [produtosManipulados, setProdutosManipulados] = useRecoilState(produtosManipuladosState)
 
+    const filtrarProduto = (preco: IFiltroPreco) => {
+        const precoMinimoFormatado = formatarPreco(preco.precoMinimo)
+        let precoMaximoFormatado = formatarPreco(preco.precoMaximo)
+
+        if (precoMaximoFormatado === 0) {
+            precoMaximoFormatado = 999
+        }
+
+        setProdutosManipulados(
+            produtos.filter(
+                produto =>
+                    produto.preco > precoMinimoFormatado &&
+                    produto.preco < precoMaximoFormatado
+            )
+        )
+    }
 
     return {
         filtrarProduto,
