ed86423e248001556358e9df59a0398716a32912
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-03 22:00:01 -0300

    :sparkles: feat: Altera hook para manipular estado global

--- src/hooks/useListaProdutos.ts
+++ src/hooks/useListaProdutos.ts
@@ -1,8 +1,24 @@
-import { useRecoilValue } from 'recoil'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
 import { listaProdutosState } from '@/state/atom'
+import { IProduto } from '@/interfaces/IProduto'
 
 export const useListaProdutos = () => {
-    const produtos = useRecoilValue(listaProdutosState)
+    const [produtos, setProdutos] = useRecoilState(listaProdutosState)
+
+    useEffect(() => {
+        axios
+            .get<IProduto[]>(
+                'https://gist.githubusercontent.com/bugan/41d60ffa23fa0c4044cc138bf670780d/raw?utm_medium=email&_hsmi=231361624&utm_content=231361624&utm_source=hs_automation'
+            )
+            .then(resposta => {
+                setProdutos(resposta.data)
+            })
+            .catch(erro => {
+                console.log(erro)
+            })
+    }, [setProdutos])
 
     return {
         produtos,
